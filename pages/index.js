0bebf1b2a1c7176f08320e8c37ca24e132ed19d8
Scott Cho <scottsgcho@gmail.com>
2019-02-26 23:57:24 +0900

    config files

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,19 @@
+//Running App
+//npm run dev
+// Shopify Partners Login
+// https://partners.shopify.com
+// For development use ngrok tunnelling 
+// ~/ngrok http 3000
+// If ngrok pro option
+// ~/ngrok http 3000 -subdomain=scottshopify
+// https://scottshopify.ngrok.io
+// React Auth Help
+// https://help.shopify.com/en/api/tutorials/build-a-shopify-app-with-node-and-react/set-up-your-app
+// To Run App
+// {forwarding address}/shopify?shop={shop name}.myshopify.com
+// http://scottshopify.ngrok.io/shopify?shop=bath-niche.myshopify.com
+
+
 const Index = () => (
     <div>
       <p>Sample app using React and Next.js</p>
