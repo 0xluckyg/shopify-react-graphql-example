a3c4f3020dc066d5fae662fffa65a2d1601d751c
Scott Cho <scottsgcho@gmail.com>
2019-03-08 03:32:13 +0900

    product selection with local storage and ResourceList

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,9 @@
 // https://polaris.shopify.com/components/get-started
 
 import { EmptyState, Layout, Page, ResourcePicker } from '@shopify/polaris';
+//Using local storage for this demo app. Should use a database in a production app
+import localStorage from 'store-js';
+import ResourceListWithProducts from '../components/resource-list';
 
 //pages/index.js file gets automatically transpiled and rendered by Next.js
 //Next.js automatically routes views that are stored in a "pages" directory using the file name
@@ -8,6 +11,7 @@ class Index extends React.Component {
     state = { open: false };
 
     render() {
+        const emptyState = !localStorage.get('ids');
         return (
                 <Page
                     primaryAction={{
@@ -23,26 +27,31 @@ class Index extends React.Component {
 						onSelection={(resources) => this.handleSelection(resources)}
 						onCancel={() => this.setState({ open: false })}
 					/>
-                    <Layout>
-                        <EmptyState
-                            heading="Select products to start"
-                            action={{
-                                content: 'Select products',
-                                onAction: () => this.setState({ open: true }),
-                            }}
-                            image="https://cdn.shopify.com/s/files/1/0757/9955/files/empty-state.svg"
-                        >
-                            <p>Select products and change their price temporarily</p>
-                        </EmptyState>
-                    </Layout>
+                    {/* Checks if there are products selected */}
+                    {emptyState ? (
+                        <Layout>
+                            <EmptyState
+                                heading="Select products to start"
+                                action={{
+                                    content: 'Select products',
+                                    onAction: () => this.setState({ open: true }),
+                                }}
+                                image="https://cdn.shopify.com/s/files/1/0757/9955/files/empty-state.svg"
+                            >
+                                <p>Select products and change their price temporarily</p>
+                            </EmptyState>                        
+                        </Layout>
+                    ) : (
+                        <ResourceListWithProducts />
+                    )}
                 </Page >
         );
     }
     handleSelection = (resources) => {
         //Passes data from the ResourcePicker
 		const idsFromResources = resources.selection.map((product) => product.id);
-      	this.setState({ open: false })
-      	console.log(resources);
+      	this.setState({ open: false });
+      	localStorage.set('ids', idsFromResources);
     };
 }
 
